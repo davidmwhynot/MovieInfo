98d73cf591c3c1bec9ac915a01f1237136f1a145
David Whynot <davidmwhynot@gmail.com>
2017-05-05 23:05:44 -0400

    finished tutorial

--- main.js
+++ main.js
@@ -24,8 +24,53 @@ function getMovies(searchText) {
         `;
       });
       $('#movies').html(output);
-    })
-    .catch((err) => {
+    }).catch((err) => {
       console.log(err);
-    });
+  });
+}
+function movieSelected(id) {
+  sessionStorage.setItem('movieId', id);
+  window.location = 'movie.html';
+  return false;
+}
+function getMovie() {
+  let movieId = sessionStorage.getItem('movieId');
+  axios.get('http://www.omdbapi.com?i=' + movieId)
+    .then((response) => {
+      console.log(response);
+      let movie = response.data;
+
+      let output = `
+        <div class="row">
+          <div class="col-md-4">
+            <img src="${movie.Poster}" class="thumbnail">
+          </div>
+          <div class="col-md-8">
+            <h2>${movie.Title}</h2>
+            <ul>
+              <li class="list-group-item">Genre: ${movie.Genre}</li>
+              <li class="list-group-item">Released: ${movie.Released}</li>
+              <li class="list-group-item">Rated: ${movie.Rated}</li>
+              <li class="list-group-item">IMDB Rating: ${movie.imdbRating}</li>
+              <li class="list-group-item">Director: ${movie.Director}</li>
+              <li class="list-group-item">Writer: ${movie.Writer}</li>
+              <li class="list-group-item">Actors: ${movie.Actors}</li>
+            </ul>
+          </div>
+        </div>
+        <div class="row">
+          <div class="well">
+            <h3>Plot</h3>
+            ${movie.Plot}
+            <hr>
+            <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View on IMDB</a>
+            <a href="index.html" class="btn btn-default">Go Back to Search</a>
+          </div>
+        </div>
+      `;
+
+      $('#movie').html(output);
+    }).catch((err) => {
+      console.log(err);
+  });
 }
